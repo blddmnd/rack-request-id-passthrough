81048df12cda78f6a3e3e43a8ccdd61ff58d5a60
Alexander Morozov <alexander.morozov@ring.com>
2018-01-02 18:05:47 +0200

    New rubocop configuration, fixed specs

--- spec/net_http_header_spec.rb
+++ spec/net_http_header_spec.rb
@@ -2,17 +2,20 @@ describe 'Net::HTTPHeader' do
   let(:app)     { ->(env) { [200, {}, [env.to_yaml]] } }
   let(:stack)   { RackRequestIDPassthrough::Middleware::Rack.new app }
   let(:request) { Rack::MockRequest.new stack }
+  let(:url)     { 'http://ring.com' }
 
   context 'with http_headers configuration' do
     it 'should append request id to outgoing headers' do
       response = request.get('/')
-      stub = stub_request(:get, 'http://example.com/').
-          with(headers: { 'Accept' => '*/*', 'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-                          'Host' => 'example.com', 'User-Agent' => 'Ruby',
-                          'Ring-Request-Id' => response.headers['Ring-Request-Id'] })
-      Net::HTTP.get_response(URI.parse('http://example.com'))
+
+      stub = stub_request(:get, url)
+             .with(headers: { 'Accept' => '*/*',
+                              'Accept-Encoding' => 'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                              'User-Agent' => 'Ruby',
+                              'Ring-Request-Id' => response.headers['Ring-Request-Id'] })
+
+      Net::HTTP.get(URI.parse(url))
       expect(stub).to have_been_requested
-      WebMock.reset!
     end
   end
 
@@ -21,9 +24,10 @@ describe 'Net::HTTPHeader' do
       RackRequestIDPassthrough.configuration.http_headers = nil
 
       request.get('/')
-      stub = stub_request(:get, 'http://example.com/')
-      stub_request(:get, 'example.com').with { |request| !request.headers.include?('Ring-Request-Id') }
-      Net::HTTP.get_response(URI.parse('http://example.com'))
+      stub = stub_request(:get, url)
+             .with { |request| !request.headers.include?('Ring-Request-Id') }
+
+      Net::HTTP.get(URI.parse(url))
       expect(stub).to have_been_requested
     end
   end
